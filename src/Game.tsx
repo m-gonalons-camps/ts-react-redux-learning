ae1430d51ea06892f48fd62c05b2272eca1e6b28
Marcos Goñalons Camps <m.gonalons.camps@gmail.com>
2017-12-30 13:07:42 +0100

    Refactoring state into game component

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,16 +1,33 @@
 import * as React from "react";
 import Board from "./components/Board";
+import { Square, TSquareValue } from "./components/Square";
 
 import "./Game.css";
 
 
-export default class Game extends React.Component {
+interface State {
+    squaresValues: TSquareValue[];
+    xIsNext: boolean;
+    winner: TSquareValue;
+}
+
+export default class Game extends React.Component<{}, State> {
+
+    public constructor(props: any) {
+        super(props);
+        this.initState();
+        this.bindings();
+    }
 
     public render(): JSX.Element {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <div className="status">{this.getBoardStatusDescription()}</div>
+                    <Board
+                        onClick={this.handleSquareClick}
+                        squaresValues={this.state.squaresValues}
+                    />
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -18,6 +35,70 @@ export default class Game extends React.Component {
                 </div>
             </div>
         );
+    }
+
+
+    private handleSquareClick(clickedSquare: Square): void {
+        const squaresValues = this.state.squaresValues.slice();
+        const clickedSquareValue = squaresValues[clickedSquare.props.squareNumber];
+
+        if (! this.canSetSquareValue(clickedSquareValue)) return;
+
+        squaresValues[clickedSquare.props.squareNumber] = this.state.xIsNext ? "X" : "O";
+
+        this.setState({
+            squaresValues,
+            xIsNext: !this.state.xIsNext,
+            winner: this.getWinner(squaresValues)
+        });
+    }
+
+    private canSetSquareValue(clickedSquareValue: TSquareValue): boolean {
+        return (
+            this.state.winner === null &&
+            clickedSquareValue === null
+        );
+    }
+
+    private getWinner(squaresValues: TSquareValue[]): TSquareValue {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (const i in lines) {
+            const [a, b, c] = lines[i];
+            if (squaresValues[a] && squaresValues[a] === squaresValues[b] && squaresValues[a] === squaresValues[c])
+                return squaresValues[a];
+        }
+
+        return null;
+    }
+
+    private getBoardStatusDescription(): string {
+        return (
+            this.state.winner ?
+            ("Winner: " + this.state.winner) :
+            ("Next player: " + (this.state.xIsNext ? "X" : "O"))
+        );
+    }
+
+    private bindings(): void {
+        this.handleSquareClick = this.handleSquareClick.bind(this);
+    }
+
+    private initState(): void {
+        this.state = {
+            squaresValues: Array(Board.totalCols * Board.totalRows).fill(null),
+            xIsNext: true,
+            winner: null
+        };
     }
 
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,37 +1,26 @@
 import * as React from "react";
 import { Square, TSquareValue } from "./Square";
 
-
-interface State {
+interface Props {
+    onClick: (clickedSquare: Square) => void;
     squaresValues: TSquareValue[];
-    xIsNext: boolean;
-    winner: TSquareValue;
 }
 
-export default class Board extends React.Component<{}, State> {
+export default class Board extends React.Component<Props> {
 
-    private readonly totalRows = 3;
-    private readonly totalCols = 3;
-
-    public constructor(props: any) {
-        super(props);
-        this.initState();
-        this.bindings();
-    }
+    public static readonly totalRows = 3;
+    public static readonly totalCols = 3;
 
     public render(): JSX.Element {
         return (
-            <div>
-                <div className="status">{this.getBoardStatusDescription()}</div>
-                {this.renderBoardRows()}
-            </div>
+            <div>{this.renderBoardRows()}</div>
         );
     }
 
     private renderBoardRows(): JSX.Element[] {
         const rows = [] as JSX.Element[];
 
-        for (let i = 0; i < this.totalRows; i += 1) {
+        for (let i = 0; i < Board.totalRows; i += 1) {
             rows.push(
                 <div className="board-row">
                     {this.renderRowSquares(i)}
@@ -45,85 +34,22 @@ export default class Board extends React.Component<{}, State> {
     private renderRowSquares(row: number): JSX.Element[] {
         const squares = [] as JSX.Element[];
 
-        for (let i = 0; i < this.totalCols; i += 1)
+        for (let i = 0; i < Board.totalCols; i += 1)
             squares.push(this.renderSquare(i, row));
 
         return squares;
     }
 
     private renderSquare(row: number, column: number): JSX.Element {
-        const squareNumber: number = column + (this.totalCols * row);
+        const squareNumber: number = column + (Board.totalCols * row);
 
         return (
             <Square
                 squareNumber={squareNumber}
-                value={this.state.squaresValues[squareNumber]}
-                onClick={this.handleClick}
+                value={this.props.squaresValues[squareNumber]}
+                onClick={this.props.onClick}
             />
         );
-    }
-
-    private handleClick(clickedSquare: Square): void {
-        const squaresValues = this.state.squaresValues.slice();
-        const clickedSquareValue = squaresValues[clickedSquare.props.squareNumber];
-
-        if (! this.canSetSquareValue(clickedSquareValue)) return;
-
-        squaresValues[clickedSquare.props.squareNumber] = this.state.xIsNext ? "X" : "O";
-
-        this.setState({
-            squaresValues,
-            xIsNext: !this.state.xIsNext,
-            winner: this.getWinner(squaresValues)
-        });
-    }
-
-    private canSetSquareValue(clickedSquareValue: TSquareValue): boolean {
-        return (
-            this.state.winner === null &&
-            clickedSquareValue === null
-        );
-    }
-
-    private getWinner(squaresValues: TSquareValue[]): TSquareValue {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-
-        for (const i in lines) {
-            const [a, b, c] = lines[i];
-            if (squaresValues[a] && squaresValues[a] === squaresValues[b] && squaresValues[a] === squaresValues[c])
-                return squaresValues[a];
-        }
-
-        return null;
-    }
-
-    private getBoardStatusDescription(): string {
-        return (
-            this.state.winner ?
-            ("Winner: " + this.state.winner) :
-            ("Next player: " + (this.state.xIsNext ? "X" : "O"))
-        );
-    }
-
-    private bindings(): void {
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    private initState(): void {
-        this.state = {
-            squaresValues: Array(this.totalCols * this.totalRows).fill(null),
-            xIsNext: true,
-            winner: null
-        };
     }
 
 }
