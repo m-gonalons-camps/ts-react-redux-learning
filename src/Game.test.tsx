ca0340c638a58ce76f78d6118d3374b8ac3adc43
Marcos Gonalons <marcos.gonalons@trivago.com>
2018-01-08 10:22:43 +0100

    Comments

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,11 +1,3 @@
-/*
-    - It must render
-    - Given a set of props, must return the correct output
-    - States and conditionals, all of them
-    - Events
-    - Edge cases
-*/
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Game from "./Game";
---
+++ src/components/Board.test.tsx
@@ -0,0 +1,5 @@
+/*
+    - It must render
+    - MUst have 2 properties: onclkick and squarevalues
+    - Must have 9 square children
+*/
